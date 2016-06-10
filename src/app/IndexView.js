4b727cd7ccb618fc08324dc9293cb89e267b4e3e
José Lobo <jloboprs@gmail.com>
2016-06-10 02:23:37 -0500

    Ocultar o mostrar el menú lateral según tamaño o preferencia

---
+++ src/app/IndexView.js
@@ -0,0 +1,39 @@
+module.exports = new (class IndexView {
+  constructor() {
+    this.btnCollapse = $('#btnCollapse');
+    this.btnCollapseHidden = $('#btnCollapseHidden');
+    this.menu = $('#menu');
+    this.layout = $('#main, #header, #footer, #nav');
+
+    this.init();
+  }
+
+  init() {
+    $(window).on('resize', e => this.onResizeWindow(e));
+    this.btnCollapse.on('click', e => this.onClickCollapse(e));
+    this.btnCollapseHidden.sideNav({closeOnClick: true});
+    this.menu.removeAttr('style');
+  }
+
+  onResizeWindow() {
+    if (window.innerWidth <= 992) {
+      this._showMenu();
+    }
+  }
+
+  onClickCollapse() {
+    if (window.innerWidth > 992) {
+      this.menu.toggleClass('hide');
+      this.layout.toggleClass('hide-menu');
+    }
+    else {
+      this._showMenu();
+      this.btnCollapseHidden.click();
+    }
+  }
+
+  _showMenu() {
+    this.menu.removeClass('hide');
+    this.layout.removeClass('hide-menu');
+  }
+})();
