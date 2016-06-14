17189e9c728335a04e92b3b4b5e2675912c41ef8
José Lobo <jloboprs@gmail.com>
2016-06-13 23:45:14 -0500

    Se mueve el código relacionado con el menú al js con el mismo nombre

--- src/app/itemMenu.js
+++ src/app/itemMenu.js
@@ -1,20 +1,18 @@
 module.exports = class ItemMenu {
   constructor() {
     const template = document.getElementById('itemMenuTemplate').import.querySelector('template');
-    const clone = document.importNode(template.content, true);
 
-    this.header = clone.querySelector('.collapsible-header').lastChild;
-    this.body = clone.querySelector('.collapsible-body');
-
-    document.getElementById('menu').appendChild(clone);
+    this.element = document.importNode(template.content, true);
+    this._header = this.element.querySelector('.collapsible-header').lastChild;
+    this._body = this.element.querySelector('.collapsible-body');
   }
 
   setHeader(header) {
-    this.header.textContent = header;
+    this._header.textContent = header;
   }
 
   setBody(...items) {
-    const ul = this.body.appendChild(document.createElement('ul'));
+    const ul = this._body.appendChild(document.createElement('ul'));
     ul.innerHTML = items.map(item => `<li><a href="#!">${item}</a></li>`).join('');
   }
 };
---
+++ src/app/menu.js
@@ -0,0 +1,38 @@
+const Plugin = require('./plugin');
+const ItemMenu = require('./itemMenu');
+const EventEmitter = require('events');
+
+module.exports = class Menu extends EventEmitter {
+  constructor() {
+    super();
+
+    this.plugin = new Plugin();
+    this.menu = document.getElementById('menu');
+    this.btnCollapse = document.getElementById('btnMenuCollapse');
+  }
+
+  init() {
+    this.menu.removeAttribute('style');
+    this.btnCollapse.addEventListener('click', (e) => this.emit('collapse', e));
+
+    const plugins = Object.keys(this.plugin.list);
+
+    for (let i = 0; i < plugins.length; i++) {
+      const item = new ItemMenu();
+      item.setHeader(plugins[i]);
+      this.add(item);
+    }
+  }
+
+  add(itemMenu) {
+    this.menu.appendChild(itemMenu.element);
+  }
+
+  show() {
+    this.menu.classList.remove('hide');
+  }
+
+  toggleShow() {
+    this.menu.classList.toggle('hide');
+  }
+};
