41c6228930fda30c15326e51cf2926c8d85fcc29
José Lobo <jloboprs@gmail.com>
2017-03-12 17:46:23 -0500

    Se mueve lógica de manejo de las vistas a domoto

--- src/app/extensionManager.js
+++ src/app/extensionManager.js
@@ -18,7 +18,7 @@ module.exports = class ExtensionManager {
     this.main = document.getElementById('main');
     this._externalLink = ExternalLink.instance;
 
-    this.installManager.on('removed', extension => this._onRemoved(extension));
+    this.installManager.on('removed', extension => extension.remove());
     this.installManager.on('installed', extension => this._loadExtension(extension));
 
     this._loadExtension(...this.components);
@@ -32,42 +32,18 @@ module.exports = class ExtensionManager {
     return this[singleton];
   }
 
-  add(extension) {
-    extension.itemMenu.on('click', () => this._changeView(extension.body));
-    extension.on('remove', () => this.remove(extension));
-    this._externalLink.apply(extension.body);
-
-    extension.body.hide();
-    if (!this._visibleBody)
-      this._changeView(extension.body);
-
-    this.menu.add(extension.itemMenu);
-    extension.body.add(this.main);
-  }
-
-  remove(extension) {
-    this.installManager.remove(extension.name);
-  }
-
-  _onRemoved(extension) {
-    extension.remove();
-
-    if (this._visibleBody === extension.body)
-      this._changeView(this.components[0].body);
-  }
-
   _loadExtension(...extensions) {
     for (const extension of extensions) {
-      if (extension)
-        extension.on('ready', () => this.add(extension));
+      if (extension) {
+        extension.on('remove', () => this.installManager.remove(extension.name));
+        extension.on('load', (view) => this._loadView(view));
+        this.menu.add(extension.itemMenu);
+      }
     }
   }
 
-  _changeView(body) {
-    if (this._visibleBody)
-      this._visibleBody.hide();
-
-    body.show();
-    this._visibleBody = body;
+  _loadView(view) {
+    this._externalLink.apply(view);
+    view.add(this.main);
   }
 };
